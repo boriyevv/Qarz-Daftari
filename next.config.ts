bd04f18b5783b3245d3d0bcab98f6a7cc72bc646
boriyevv <boriyevdev08@gmail.com>
2026-01-12 01:36:39 +0500

    --turbopack build mode replaced with --webpack

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,7 @@ const withPWA = withPWAInit({
 const nextConfig: NextConfig = {
   // Sizning boshqa konfiguratsiyalaringiz...
   reactStrictMode: true,
+  
 };
 
 export default withPWA(nextConfig);
--- src/data/folders.ts
+++ src/data/folders.ts
@@ -3,19 +3,9 @@ import { Folder } from "@/types/folder";
 export const folders: Folder[] = [
   {
     id: "default",
+    shopId:'1',
     name: "Qarzdorlar",
-    type: "default",
-    description: "Barcha umumiy qarzlar",
     createdAt: new Date().toISOString(),
-    isActive: true,
-    isSystem: true,
-  },
-  {
-    id: "1",
-    name: "Ustalar",
-    type: "custom",
-    createdAt: new Date().toISOString(),
-    isActive: true,
-    isSystem: false,
+    isDefault: true,
   },
 ];
